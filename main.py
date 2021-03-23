139d8eb4619d13bea60d9cf92db4f267984fc6c0
stasandrey <staschenyuk.andrey@gmail.com>
2021-03-23 21:49:04 +0300

    Получил данные по Барановичам с openweathermap.org

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+import requests
 
-# Press the green button in the gutter to run the script.
+url_data = "https://api.openweathermap.org/data/2.5/weather"
+key = "dbc2bb795a4c0ddb04c9cc2772b4809f"
+p = {'q': 'baranovichi,by', 'appid': key}
+kelvin = 273.15
+
+class Weather:
+    def __init__(self):
+        pass
+
+    def run(self):
+        r = requests.get( url_data, params = p )
+
+        return r
+
+
 if __name__ == '__main__':
     print('Начало работы')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    w = Weather()
+    res = w.run()
+    print(res)
+    res = res.json()
+    print(res)
+    print('Температура', res['main']['temp'] - kelvin)
+    print('Ощущается как:', res['main']['feels_like'] - kelvin)
+    print('Ветер', res['wind']['speed'])
+    print('Направление', res['wind']['deg'])
